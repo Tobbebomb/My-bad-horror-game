11ca71c3effd32f36078960f0582b9b72d137f78
skeleium123 <hhhh>
2025-03-21 10:36:08 +0100

    a

--- monster_test.gd
+++ monster_test.gd
@@ -1,11 +1,25 @@
 extends CharacterBody3D
 
+var player = null
+
+@export var player_path : NodePath
+
+@onready var nav_agent = $Test/NavigationAgent3D
+
+const SPEED = 3.0
+
+func _ready():
+	player = get_node(player_path)
 
 
-
-const SPEED = 5.0
-
-
-
-func _physics_process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	velocity = Vector3.ZERO
+	
+	#navigation
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	move_and_slide()
